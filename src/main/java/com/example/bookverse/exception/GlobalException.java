a94ea0a1a6accee1bb9fadb20181bdfbf351fae8
PhanLaThanhPhong <phanlathanhphong19@gmail.com>
2025-01-19 19:42:29 +0700

    19/1/2025 update User and Role

--- src/main/java/com/example/bookverse/exception/GlobalException.java
+++ src/main/java/com/example/bookverse/exception/GlobalException.java
@@ -1,6 +1,7 @@
 package com.example.bookverse.exception;
 
 import com.example.bookverse.domain.response.RestResponse;
+import com.example.bookverse.exception.role.ExistRoleNameException;
 import com.example.bookverse.exception.user.ExistUsernameException;
 import com.example.bookverse.exception.global.IdInvalidException;
 import org.springframework.http.HttpStatus;
@@ -21,7 +22,7 @@ public class GlobalException {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
     }
 
-
+    // Handle User
     @ExceptionHandler(value = {
             ExistUsernameException.class,
     })
@@ -30,6 +31,18 @@ public class GlobalException {
         res.setStatus(HttpStatus.BAD_REQUEST.value());
         res.setMessage(ex.getMessage());
         res.setError("Username already exist");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
+    }
+
+    // Handle Role
+    @ExceptionHandler(value = {
+            ExistRoleNameException.class,
+    })
+    public ResponseEntity<RestResponse<Object>> handleExistRoleNameException(Exception ex) {
+        RestResponse<Object> res = new RestResponse<>();
+        res.setStatus(HttpStatus.BAD_REQUEST.value());
+        res.setMessage(ex.getMessage());
+        res.setError("RoleName already exist");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
     }
 }
